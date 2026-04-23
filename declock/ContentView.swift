a4d92e366f5845eec6f78dc40ac880d32f8eb290
fukuyori <self@spumoni.org>
2026-04-23 14:13:16 +0900

    Initial Commit

---
+++ declock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  declock
+//
+//  Created by 福寄典明 on 2026/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ declock/declockApp.swift
@@ -0,0 +1,17 @@
+//
+//  declockApp.swift
+//  declock
+//
+//  Created by 福寄典明 on 2026/04/23.
+//
+
+import SwiftUI
+
+@main
+struct declockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
